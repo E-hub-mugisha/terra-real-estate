62009a2a7a86fa5bb9af4290dc18ca4951531f37
eric mugisha <kabosierik@gmail.com>
2026-03-25 12:57:28 +0200

    cons serve

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -23,7 +23,7 @@ class Service extends Model
 
     public function subcategory()
     {
-        return $this->belongsTo(ServiceSubcategory::class, 'service_subcategory_id');
+        return $this->belongsTo(ServiceSubCategory::class, 'service_subcategory_id');
     }
 
     public function houses()
