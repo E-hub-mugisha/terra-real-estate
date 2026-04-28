e350fb874401e87a7f50ce1dcb74e52ad0c94ac5
eric mugisha <kabosierik@gmail.com>
2026-04-28 11:48:03 +0200

    status location

--- resources/views/front/buy/home-details.blade.php
+++ resources/views/front/buy/home-details.blade.php
@@ -1411,7 +1411,7 @@
                             <svg viewBox="0 0 24 24" fill="currentColor">
                                 <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
                             </svg>
-                            {{ $home->address }}@if($home->city), {{ $home->city }}@endif
+                            {{ $home->sector }},{{ $home->district }}@if($home->province), {{ $home->province }}@endif
                         </div>
                         <div class="pd-chips">
                             @if($home->bedrooms)
@@ -1516,11 +1516,11 @@
                             @endif
                             <tr>
                                 <td>City</td>
-                                <td>{{ $home->city ?? '—' }}</td>
+                                <td>{{ $home->district ?? '—' }}</td>
                             </tr>
                             <tr>
-                                <td>Zip Code</td>
-                                <td>{{ $home->zip_code ?? '—' }}</td>
+                                <td>Sector</td>
+                                <td>{{ $home->sector ?? '—' }}</td>
                             </tr>
                         </table>
                     </div>
