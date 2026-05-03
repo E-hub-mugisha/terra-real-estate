77fa20b3ec9132339ccdaeb41a718618e297ea9e
eric mugisha <kabosierik@gmail.com>
2026-05-03 16:06:07 +0200

    browse

--- resources/views/includes/front/footer.blade.php
+++ resources/views/includes/front/footer.blade.php
@@ -586,7 +586,7 @@
                         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                             <path d="M5 12h14M12 5l7 7-7 7" />
                         </svg>
-                        Listings
+                        Browse Properties
                     </a>
                 </div>
             </div>
